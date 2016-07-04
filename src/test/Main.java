4790854adcd7cb03801c968aa87fb90de9f77d5f
super <super@HyeonWoo>
2016-07-04 11:19:04 +0900

    boolean test

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,9 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("classpath:applicationContent.xml");
-		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("스프링");
-		System.out.println(msg);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+//		String msg = g1.greet("스프링");
+		System.out.println("(g1==g2)=" + (g1 == g2));
 		ctx.close();
 				
 	}
